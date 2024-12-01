670a582fe1cff97feb97f209e3fc86aa52a6f5be
Renette Ros <supergirl0894+git@gmail.com>
2024-12-01 07:07:07 +0200

    Solve day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,12 +1,27 @@
 package day1
 
 import helper.Debug
+import kotlin.math.abs
 
 fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
-    return 0
+    val (left, right) = parseInput(text)
+
+    return left.sorted().zip(right.sorted()).sumOf { (l, r) -> abs(l - r) }
+}
+
+private fun parseInput(text: String): Pair<MutableList<Int>, MutableList<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    val regex = " +".toRegex()
+    text.trim().lines().map { it.split(regex) }.forEach { (l, r) ->
+        left.add(l.toInt())
+        right.add(r.toInt())
+    }
+    return Pair(left, right)
 }
 
 
 fun solveB(text: String, debug: Debug = Debug.Disabled): Int {
-    return 0
+    val (left, right) = parseInput(text)
+    return left.sumOf { l -> l * right.count { r -> r == l } }
 }
--- src/test/kotlin/day1/Day1KtTest.kt
+++ src/test/kotlin/day1/Day1KtTest.kt
@@ -2,7 +2,6 @@ package day1
 
 import helper.Debug
 import helper.readDayFile
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -14,24 +13,16 @@ internal class Day1KtTest {
     @Test
     fun sample1() {
         val text = """
-            |
-        """.trimMargin().trimEnd()
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        """.trimIndent().trimEnd()
 
-        assertEquals(0, solveA(text, Debug.Enabled))
-        assertEquals(0, solveB(text, Debug.Enabled))
-    }
-
-    @Test
-    @Ignore
-    fun sample2() {
-//        val text = readDayFile(day, "sample2.in").readText().trimEnd()
-
-        val text = """
-            |
-        """.trimMargin().trimEnd()
-
-        assertEquals(0, solveA(text, Debug.Enabled))
-        assertEquals(0, solveB(text, Debug.Disabled))
+        assertEquals(11, solveA(text, Debug.Enabled))
+        assertEquals(31, solveB(text, Debug.Enabled))
     }
 
     @Test
@@ -40,10 +31,10 @@ internal class Day1KtTest {
 
         val solveA = solveA(lines)
         println("A: $solveA")
-        assertEquals(0, solveA)
+        assertEquals(1506483, solveA)
 
         val solveB = solveB(lines)
         println("B: $solveB")
-        assertEquals(0, solveB)
+        assertEquals(23126924, solveB)
     }
 }
