3fcc2132ccbed6e348618e7000f949dba8b871e8
Renette Ros <supergirl0894+git@gmail.com>
2024-12-11 17:44:57 +0200

    Day 11 Original solution

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -2,6 +2,29 @@ package day11
 
 import helper.Debug
 
+// Original solution for part 1
+fun solveA(text: String, debug: Debug = Debug.Disabled, reps: Int = 25): Int {
+    var list = text.split(" ").map { it.toLong() }
+    repeat(reps) {
+        list = list.flatMap { stone ->
+            if (stone == 0L) {
+                listOf(1)
+            } else {
+                val str = "$stone"
+                if (str.length % 2 == 0) {
+                    listOf(
+                        str.substring(0..<str.length / 2).toLong(),
+                        str.substring(str.length / 2).toLong()
+                    )
+                } else {
+                    listOf(stone * 2024)
+                }
+            }
+        }
+    }
+    return list.size
+}
+
 fun solve(text: String, debug: Debug = Debug.Disabled, reps: Int): Long {
     val list = text.split(" ").map { it.toLong() }
     val cache = (1..reps).associateWith { mutableMapOf<Long, Long>() }
